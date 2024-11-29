0c7666de7030911f7fe4e7caf81df5f417a5ee9f
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2024-11-29 15:57:31 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TrashEntity} from './trash.entity';
+import { TrashModule } from './trash/trash.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'trash',
+      entities: [TrashEntity],
+      synchronize: true
+    }),
+    TrashModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
